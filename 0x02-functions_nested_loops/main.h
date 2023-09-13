7b3ee1fe46e41350afa1266deead71098d1e4a25
mohamedkamal2211 <elrhmany555@gmail.com>
2023-09-12 19:53:45 -0700

    s

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,5 +14,9 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+alx-low_level_programming
+alx-low_level_programming
 
 #endif
